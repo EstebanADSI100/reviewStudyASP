f7919f837dc271896e9c4ebac1f4fa2335f80b8f
Esteban <esteban100_10@hotmail.com>
2019-12-18 19:16:11 -0500

    Se Implementarion las funcionalidades de EditorTemplates y la utilización de DropDownList

---
+++ reviewStudyASP/Controllers/DropDownListController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using reviewStudyASP.Services;
+
+namespace reviewStudyASP.Controllers
+{
+    public class DropDownListController : Controller
+    {
+        // GET: DropDownList
+        public ActionResult drop()
+        {
+            ViewBag.estado = listarEstado();
+            return View();
+        }
+        public List<SelectListItem> listarEstado()
+        {
+            return new List<SelectListItem>()
+                {
+                    new SelectListItem()
+                    {
+                        Text = "Activo",
+                        Value = "1"
+                    },
+
+                    new SelectListItem()
+                    {
+                        Text = "Inactivo",
+                        Value = "0"
+                    }
+                };
+        }
+    }
+}
---
+++ reviewStudyASP/Services/methodsEditorPersona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using reviewStudyASP.Models;
+
+namespace reviewStudyASP.Services
+{
+    public class methodsEditorPersona
+    {
+        public Persona listarPersona()
+        {
+            return new Persona()
+            {
+                Id = 1,
+                Nombre = "Perro Loco",
+                Empleado = true,
+                FechaNacimiento = new DateTime(1991, 01, 02)
+            };
+        }
+    }
+}
