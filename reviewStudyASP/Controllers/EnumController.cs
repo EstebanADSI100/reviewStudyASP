7e295059b2ab45d15b78e8a15abb6964badd4a64
Esteban <esteban100_10@hotmail.com>
2019-12-18 22:10:08 -0500

    Se Implemento las funcionalidades de Enum, plasmado en un Select.

---
+++ reviewStudyASP/Controllers/EnumController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using reviewStudyASP.Models;
+
+namespace reviewStudyASP.Controllers
+{
+    public class EnumController : Controller
+    {
+        // GET: Enum
+        public ActionResult ActionEnum()
+        {
+            ViewBag.enumSelect = ToListSelectListItem<ResultadoOperacion>();
+            return View();
+        }
+
+        public List<SelectListItem> ToListSelectListItem<T>()
+        {
+            var t = typeof(T);
+
+            if (!t.IsEnum) 
+            { 
+                throw new ApplicationException("Tipo debe ser enum"); 
+            }
+
+            var members = t.GetFields(BindingFlags.Public | BindingFlags.Static);
+
+            var result = new List<SelectListItem>();
+
+            foreach (var member in members)
+            {
+                var attributeDescription = member.GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute),
+                    false);
+                var descripcion = member.Name;
+
+                if (attributeDescription.Any())
+                {
+                    descripcion = ((System.ComponentModel.DescriptionAttribute)attributeDescription[0]).Description;
+                }
+
+                var valor = ((int)Enum.Parse(t, member.Name));
+                result.Add(new SelectListItem()
+                {
+                    Text = descripcion,
+                    Value = valor.ToString()
+                });
+            }
+            return result;
+        }
+    }
+}
---
+++ reviewStudyASP/Models/ResultadoOperacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace reviewStudyASP.Models
+{
+    public enum ResultadoOperacion
+    {
+        [Description("Activo")]
+        activo = 1,
+
+        [Description("Inactivo")]
+        inactivo = 0,
+
+        [Description("Bloqueado por fraude")]
+        bloqueado = 2
+    }
+}
