46efaf7973bfd98c4766c552d1aff968f36bfb11
Esteban <esteban100_10@hotmail.com>
2019-12-24 15:22:22 -0500

    Confuguracón de la página principal del sistema de información, además analisis de todos los action del controlador, utilizando una clase C# como Modelo, implementado con Scalffolding.

---
+++ reviewStudyASP/Controllers/ScalffoldingController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using reviewStudyASP.Models;
+
+namespace reviewStudyASP.Controllers
+{
+    public class ScalffoldingController : Controller
+    {
+        private reviewStudyASPContext db = new reviewStudyASPContext();
+
+        // GET: Scalffolding
+        public ActionResult Index()
+        {
+            return View(db.Peliculas.ToList());
+        }
+
+        // GET: Scalffolding/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // GET: Scalffolding/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Scalffolding/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,titulo,estaEnCartelera,genero,sinopsis")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peliculas.Add(pelicula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Scalffolding/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Scalffolding/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,titulo,estaEnCartelera,genero,sinopsis")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pelicula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Scalffolding/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = db.Peliculas.Find(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Scalffolding/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pelicula pelicula = db.Peliculas.Find(id);
+            db.Peliculas.Remove(pelicula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ reviewStudyASP/Models/Pelicula.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace reviewStudyASP.Models
+{
+    public class Pelicula
+    {
+        public int id { get; set; }
+        
+        [Required]
+        [StringLength(130)]
+        public string titulo { get; set; }
+        public bool estaEnCartelera { get; set; }
+
+        [StringLength(120)]
+        public string genero { get; set; }
+
+        [StringLength(1000)]
+        public string sinopsis { get; set; } 
+    }
+}
